d89820d29aec0c31c958baaa68abab07ae05e53c
Javier Eduardo <javiereduardo55xd@gmail.com>
2024-08-28 00:31:06 -0700

    arreglado la inplemnetacion

--- src/App.java
+++ src/App.java
@@ -1,31 +1,43 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Archivo arc1 = new Archivo();
+
         Venta venta1 = new Venta("1","10/10/10");
         venta1.AgregarLinea(1,1,1);
         venta1.AgregarLinea(2,3,4);
-        venta1.AgregarLinea(3,5,7);
-        venta1.AgregarLinea(4,1,1);
         venta1.AgregarLinea(5,3,4);
         venta1.AgregarLinea(6,5,7);
         arc1.guardar(venta1);
         venta1 = null;
+
         Venta venta2 = new Venta("2","11/11/11");
         venta2.AgregarLinea(1,1,1);
         venta2.AgregarLinea(2,3,3);
         venta2.AgregarLinea(3,7,6);
-        venta2.AgregarLinea(4,2,1);
-        venta2.AgregarLinea(5,3,4);
-        venta2.AgregarLinea(6,5,7);
-        venta2.AgregarLinea(7,3,4);
         venta2.AgregarLinea(8,1,6);
         venta2.AgregarLinea(9,5,2);
         arc1.guardar(venta2);
         venta2 = null;
+
+        Venta venta3 = new Venta("3","12/12/12");
+        venta3.AgregarLinea(1,1,1);
+        venta3.AgregarLinea(2,3,3);
+        arc1.guardar(venta3);
+
+        Venta venta4 = new Venta("4","10/12/14");
+        venta4.AgregarLinea(1,1,1);
+        venta4.AgregarLinea(2,3,3);
+        arc1.guardar(venta4);
+        
         venta1 = arc1.recuperar("1");
         venta2 = arc1.recuperar("2");
         arc1.borrar("1");
-        System.out.println(venta1);
-        System.out.println(venta2);
+        arc1.guardar(venta1);
+        arc1.guardar(venta2);
+        arc1.guardar(venta3);
+        arc1.guardar(venta4);
+        for (Venta venta : arc1.obtenerVentas()) {
+            System.out.println(venta);
+        }
     }
 }
--- src/Archivo.java
+++ src/Archivo.java
@@ -1,11 +1,14 @@
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 public class Archivo {
-    FileOutputStream escritura;
-    ObjectOutputStream flujosalida;
+    private FileOutputStream escritura;
+    private ObjectOutputStream flujosalida;
     public Archivo(){
         try {
             escritura = new FileOutputStream("ventas.txt");
@@ -27,14 +30,47 @@ public class Archivo {
             ObjectInputStream flujoentrada = new ObjectInputStream(lectura);
             while (true) {
                 Venta venta = (Venta) flujoentrada.readObject();
-                if (venta.getFolio().equals(folio)) 
+                if (venta.getFolio().equals(folio)) {
+                    lectura.close();
+                    flujoentrada.close();
                     return venta;
+                }
             }
         } catch (Exception e) {
             return null;
         }
     }
     public void borrar(String folio) {
+        File archivo = new File("ventas.txt");
+        File archivoAux = new File("ventasAux.txt");
+        try {
+            FileInputStream lectura = new FileInputStream(archivo);
+            ObjectInputStream flujoentrada = new ObjectInputStream(lectura);
+            FileOutputStream escrituraAux = new FileOutputStream(archivoAux);
+            ObjectOutputStream flujosalidaAux = new ObjectOutputStream(escrituraAux);
+            while (true) {
+                try {
+                    Venta venta = (Venta) flujoentrada.readObject();
+                    if (!venta.getFolio().equals(folio)) 
+                        flujosalidaAux.writeObject(venta);
+                } catch (Exception e) {
+                    break;
+                }
+            }
+            lectura.close();
+            flujoentrada.close();
+            escritura.close();
+            flujosalida.close();
+            escrituraAux.close();
+            flujosalidaAux.close();
+            Files.move(archivoAux.toPath(), archivo.toPath(),StandardCopyOption.REPLACE_EXISTING);
+            escritura = new FileOutputStream("ventas.txt",true);
+            flujosalida = new ObjectOutputStream(escritura);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public ArrayList<Venta> obtenerVentas() {
         ArrayList<Venta> ventas = new ArrayList<>();
         try {
             FileInputStream lectura = new FileInputStream("ventas.txt");
@@ -42,23 +78,16 @@ public class Archivo {
             while (true) {
                 try {
                     Venta venta = (Venta) flujoentrada.readObject();
-                    if (!venta.getFolio().equals(folio)) 
-                        ventas.add(venta);
+                    ventas.add(venta);
                 } catch (Exception e) {
                     break;
                 }
             }
+            lectura.close();
+            flujoentrada.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        try {
-            escritura = new FileOutputStream("ventas.txt");
-            flujosalida = new ObjectOutputStream(escritura);
-            for (Venta venta : ventas) {
-                guardar(venta);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        return ventas;
     }
 }
